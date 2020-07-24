88d2d3f9f60c8f609852af4ed5c3d162cbc0720d
Benjoe Vidal <benjoe22@gmail.com>
2020-07-24 21:52:37 +0800

    Initial commit

---
+++ CleanSwiftApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwiftApp
+//
+//  Created by Benjoe Vidal on 7/24/20.
+//  Copyright © 2020 BRV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
